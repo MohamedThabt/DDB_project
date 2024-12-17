27f814ac7ccfa1acff20a941210f4a7e687034b9
MOhamed Thabet <mohamedthabetabdelhalem_sd23@fcis.bsu.edu.eg>
2024-12-17 13:49:09 +0200

    Change user role field to enum type for better role management

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('first_name');            
             $table->string('last_name');
             $table->string('email')->unique();
-            $table->string('role');
+            $table->enum('type', ['student','instructor','admin'])->default('student');
             $table->integer('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
